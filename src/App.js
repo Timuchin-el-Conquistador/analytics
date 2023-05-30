b2e44a0d3e215cb3dfc87c5b9e1f91d39899015f
Chingiz <hemidovcingiz183@gmail.com>
2023-05-30 18:20:37 +0400

    pdf template for survey analytics

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { withRouter } from "react-router-dom";
+import Options from "./pages/options";
+import Option from "./pages/option";
+import Analytics from "./pages/analytics";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    url: this.props.location.search,
+  };
+
+  render() {
+    const { url } = this.state;
+    const page = new URLSearchParams(url).get("page");
+    const id = new URLSearchParams(url).get("id");
+    const question = new URLSearchParams(url).get("question");
+    return (
+      <div className="App">
+        {page === "survey" ? <Analytics id={id}/> : page === "options" ? (
+          <Options id={id} />
+        ) : (
+          <Option id={id} question={question} />
+        )}
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withRouter(App);
---
+++ src/pages/option.js
@@ -0,0 +1,130 @@
+import React from "react";
+import axios from "axios";
+import classes from "../assets/css/index.module.scss";
+import BarDefault from "./apexcharts/bar-default";
+import Pie from "./apexcharts/pie";
+
+class Option extends React.Component {
+  state = {
+    options: {},
+  };
+  componentWillMount() {
+    const { id, question } = this.props;
+    console.log(id, question);
+    axios
+      .get(
+        `https://backendapp.murmurcars.com/api/v1/admin/survey-answer-analytics?id=${id}&question=${question}`
+      )
+      .then((response) => {
+        const { Analytics } = response.data;
+        console.log(Analytics);
+        this.setState({
+          ...this.state,
+          options: Analytics,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  renderAnalytics = (options, optionsState) => {
+    const analytics = [];
+
+    options.forEach((analytic, i) => {
+      console.log(optionsState);
+      console.log(analytic);
+      console.log(optionsState[analytic]);
+
+      analytics.push(
+        <div className={classes.no_block_shadow} key={i}>
+          <h1 className={classes.question}>Option: {analytic}</h1>
+
+          <div className="d-flex ">
+            <div className={`${classes.pies}`}>
+              <div className={classes.survey_info}>
+                <h1 className={classes.audience_segment}>Gender</h1>
+              </div>
+              <div className={classes.chart_contaioner}>
+                <Pie
+                  series={Object.values(optionsState[analytic].gender)}
+                  categories={Object.keys(optionsState[analytic].gender)}
+                  labels={true}
+                />
+              </div>
+            </div>
+
+            <div className={classes.pies}>
+              <div className={classes.survey_info}>
+                <h1>Age</h1>
+              </div>
+              <div className={classes.chart_contaioner}>
+                <BarDefault
+                  categories={Object.keys(optionsState[analytic].age)}
+                  series={Object.values(optionsState[analytic].age)}
+                  xLabels={true}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="d-flex">
+            <div className={`${classes.pies}`}>
+              <div className={classes.survey_info}>
+                <h1 className={classes.audience_segment}>Income</h1>
+              </div>
+              <div className={classes.chart_contaioner}>
+                <Pie
+                  series={Object.values(optionsState[analytic].income)}
+                  categories={Object.keys(optionsState[analytic].income)}
+                  labels={true}
+                />
+              </div>
+            </div>
+            <div className={classes.pies}>
+              <div className={classes.survey_info}>
+                <h1>Interest</h1>
+              </div>
+              <div className={classes.chart_contaioner}>
+                <BarDefault
+                  categories={Object.keys(optionsState[analytic].interest)}
+                  series={Object.values(optionsState[analytic].interest)}
+                  xLabels={true}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="d-flex">
+            <div className={classes.pies}>
+              <div className={classes.survey_info}>
+                <h1>Location</h1>
+              </div>
+              <div className={classes.chart_contaioner}>
+                <BarDefault
+                  categories={Object.keys(optionsState[analytic].location)}
+                  series={Object.values(optionsState[analytic].location)}
+                  xLabels={true}
+                  for="location"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    });
+
+    return analytics;
+  };
+
+  render() {
+    const { options: optionsState } = this.state;
+    const options = Object.keys(optionsState);
+    return (
+      <div className={` ${classes.flexbox} ${classes.flexbox_2}`}>
+        <h1>{this.props.question}</h1>
+        {this.renderAnalytics(options, optionsState)}
+      </div>
+    );
+  }
+}
+
+export default Option;
